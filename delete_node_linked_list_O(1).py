ae6ada840dbf1de2e55a1d35cab72ee3353add83
chefmohima <38052670+chefmohima@users.noreply.github.com>
2019-06-06 00:34:32 +0530

    delete node in O(1) time without a prev node pointer

---
+++ delete_node_linked_list_O(1).py
@@ -0,0 +1,73 @@
+# delete node in O(1) time
+# see the delete node method
+# no need of prev node
+# technique: copy next node's data into current node and delete the next node
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+    def get_data(self):
+        return self.data
+        
+    def set_data(self,data):
+        self.data = data
+        
+    def get_next(self):
+        return self.next
+        
+    def set_next(self,node):
+        self.next = node
+        
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail = None
+        
+    def get_head(self):
+        return self.head
+        
+    def get_tail(self):
+        return self.tail
+        
+    def set_head(self,node):
+        self.head = node
+    
+    def set_tail(self,node):
+        self.tail = node
+        
+    def append(self,node):
+        # add node at tail
+        if self.head is None:
+            self.head = node
+        else:
+            self.tail.next=node
+        self.tail = node
+        
+    def delete_node(self,node):
+        # in O(1) time, no prev node needed
+        # copy next node's data into the current node, then delete the next node
+        node.set_data(node.next.get_data())
+        node.next  = node.next.next
+        
+        
+        
+            
+   
+        
+ll = LinkedList()
+node1 = Node(0)
+node2 = Node(1)
+node3 = Node(0)
+node4 = Node(2)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+ll.set_head(node1)
+ll.set_tail(node4)
+ll.append(Node(5))
+#ll.delete_node_with_value(5)
+ll.delete_node(node1)
+
+
